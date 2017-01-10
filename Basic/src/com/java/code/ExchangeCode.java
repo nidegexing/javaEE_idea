d48320088d0867ef098c1fb8563804d832093b8b
nidegexing <nidegexing@163.com>
2017-01-10 23:49:10 +0800

    wakaka2

--- Basic/src/com/java/code/ExchangeCode.java
+++ Basic/src/com/java/code/ExchangeCode.java
@@ -5,5 +5,7 @@ public class ExchangeCode {
 	public static void main(String[] args) throws Exception {
 		String str = "�Ҳ���ָ���ĳ���";
 		System.out.println(new String(str.getBytes("utf-8"),"utf-8"));
+
+
 	}
 }
