1ced2c46f0208bac4c2b2a82ca1fb5fba3f56a7b
nidegexing <nidegexing@163.com>
2017-01-13 11:21:45 +0800

    更新Procedures.java

--- Basic/src/com/oracle/proce/Procedures.java
+++ Basic/src/com/oracle/proce/Procedures.java
@@ -4,6 +4,10 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /***
  *
@@ -13,13 +17,34 @@ import java.sql.ResultSet;
  *
  */
 public class Procedures {
-	public static void main(String[] args) {
-		new Procedures().callRouting("20161201", "20161227");
+	public static void main(String[] args) throws Exception {
+		String strDate = "20170111";
+		Date cuurDate = new Date();
+		DateFormat format = new SimpleDateFormat("yyyyMMdd");
+		String s;
+		// 字符串-->Date
+		Date date = format.parse(strDate);
+		System.out.println("字符串-->Date:" + date.toString());
+		// 字符串-->字符串
+		s = format.format(format.parse(strDate));
+		System.out.println("字符串-->字符串:" + s);
+		// Date-->字符串
+		s = format.format(cuurDate);
+		System.out.println("Date-->字符串:" + s);
+		// Date-->Date
+		Date date2 = format.parse(format.format(date));
+		System.out.println("Date-->Date:" + date2.toString());
+		// Date-->java.sql.Date
+		java.sql.Date sd = new java.sql.Date(cuurDate.getTime());
+		System.out.println("Date-->java.sql.Date:" + sd.toString());
+		// 日期计算
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		System.out.println("NextMonthDate:" + cal.get(Calendar.MONTH + 1));
 	}
 
-	private void callRouting(String startDate, String endDate) {
+	public void callRouting(String startDate, String endDate) {
 		try {
-			// 来自class12.jar的JDBC
 			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String url = "jdbc:oracle:thin:@10.88.21.10:1521:orcl";
@@ -46,19 +71,39 @@ public class Procedures {
 		}
 	}
 
-    @SuppressWarnings("unused")
-	public void GED() throws Exception {
-		Class c = Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection conn = DriverManager.getConnection(
-				"jdbc:oracle:thin:@localhost:1521:ORCL", "HH", "HH");
-		CallableStatement cstmt = conn
-				.prepareCall("{call TEST_CURSOR.TEST(?)}");
-		cstmt.registerOutParameter(1, oracle.jdbc.OracleTypes.CURSOR); // 输出参数需要注册
-		cstmt.execute();
-		ResultSet rs = (ResultSet) cstmt.getObject(1); // 注意是getObject,没有getCursor这种方法
-		while (rs.next()) {
-			System.out.println("id:" + rs.getInt(1) + " name:"
-					+ rs.getString(2) + " grade:" + rs.getInt(3));
+	public void callPersonDetail(String currDate) {
+		Connection conn = null;
+		CallableStatement proc = null;
+		try {
+			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@10.88.158.39:1521:oracle";
+			String username = "crm_zjl";
+			String password = "crm_zjl";
+			conn = DriverManager.getConnection(url, username, password);
+			proc = conn.prepareCall("{ Call KPG_PERSONDETAIL.SP_CBJS(?) }");
+			// java.sql.Date date = java.sql.Date.valueOf(currDate);
+			SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(df.parse(currDate));
+			// 获取前一天的日期
+			calendar.add(Calendar.DATE, -1);
+			System.out.println("sendDate:" + df.format(calendar.getTime()));// 今天的日期
+			// Date d = df.parse(currDate);
+			proc.setDate(1, new java.sql.Date(calendar.getTime().getTime()));
+			proc.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (proc != null) {
+					proc.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+			}
 		}
 	}
 }
